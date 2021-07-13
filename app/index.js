02e13ffe92c7afd3465eb75bcdd742df83a3eefc
jeff-h <jeffvh@outlook.com>
2021-07-13 20:33:27 +1000

    add invisible calories and heart rate elements

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import document from 'document';
 import { preferences } from 'user-settings';
 import { today as userActivityToday } from 'user-activity';
 import { HeartRateSensor } from 'heart-rate';
+import { BodyPresenceSensor } from 'body-presence';
+import { display } from 'display';
 import { battery } from 'power';
 import { zeroPad, DAY_ABBREVIATIONS, MONTH_ABBREVIATIONS } from '../common/utils';
 
@@ -13,6 +15,39 @@ clock.granularity = 'minutes';
 const dateElement = document.getElementById('date');
 const timeElement = document.getElementById('time');
 const stepsElement = document.getElementById('steps');
+const heartRateElement = document.getElementById('heart-rate');
+const caloriesElement = document.getElementById('calories');
+
+if (HeartRateSensor) {
+  const heartRateSensor = new HeartRateSensor();
+  heartRateSensor.start();
+}
+
+// Disable HRS when watch is not on wrist
+if (BodyPresenceSensor && heartRateSensor) {
+  const body = new BodyPresenceSensor();
+  body.addEventListener('reading', () => {
+    if (body.present) {
+      heartRateSensor.start();
+    } else {
+      heartRateSensor.stop();
+    }
+  });
+  body.start();
+}
+
+// Disable HRS when screen is off
+if (display && heartRateSensor) {
+  display.addEventListener('change', () => {
+    if (heartRateSensor != null) {
+      if (display.on) {
+        heartRateSensor.start();
+      } else {
+        heartRateSensor.stop();
+      }
+    }
+  });
+}
 
 const getHours = (today) => {
   let hours = today.getHours();
@@ -32,13 +67,10 @@ const setDate = (now) => {
   dateElement.text = `${day}, ${date} ${month}`;
 };
 
-const setSteps = () => {
-  if (userActivityToday.adjusted != null) {
-    stepsElement.text = userActivityToday.adjusted.steps;
-  } else {
-    stepsElement.text = '--';
-  }
-};
+const setActivityValue = (element, value) => (element.text = value ?? '--');
+const setSteps = () => setActivityValue(stepsElement, userActivityToday.adjusted?.steps);
+const setHeartRate = () => setActivityValue(heartRateElement, heartRateSensor?.heartRate);
+const setCalories = () => setActivityValue(caloriesElement, userActivityToday.adjusted?.steps);
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -46,4 +78,6 @@ clock.ontick = (evt) => {
   setTime(now);
   setDate(now);
   setSteps();
+  setHeartRate();
+  setCalories();
 };
