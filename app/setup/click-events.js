845e4dcd3050e2dd8f4e907d8a3f92e5d5bd39a7
jeff-h <jeffvh@outlook.com>
2021-07-17 22:15:31 +1000

    update colour of arc based on measurement visible

--- app/setup/click-events.js
+++ app/setup/click-events.js
@@ -1,20 +1,32 @@
 import document from 'document';
-import { MEASUREMENT_CONTAINER_IDS } from '../../common/utils';
+import { MEASUREMENT_CONTAINER_IDS, ARC_COLOURS, BACKGROUND_ARC_COLOURS } from '../../common/utils';
 
 const initiateClickEvents = () => {
   const rootElement = document.getElementById('root');
+  const secondsBackgroundArc = document.getElementById('seconds-background-arc');
+  const secondsArc = document.getElementById('seconds-arc');
   let currentIndex = 0;
+
+  const setMeasurementsVisibility = (currentId, nextId) => {
+    const currentEle = document.getElementById(currentId);
+    const nextEle = document.getElementById(nextId);
+
+    currentEle.style.visibility = 'hidden';
+    nextEle.style.visibility = 'visible';
+  };
+
+  const setArcColours = (measurementContainerId) => {
+    secondsBackgroundArc.style.fill = BACKGROUND_ARC_COLOURS[measurementContainerId];
+    secondsArc.style.fill = ARC_COLOURS[measurementContainerId];
+  };
 
   const displayNextActvity = () => {
     const currentId = MEASUREMENT_CONTAINER_IDS[currentIndex];
     const nextIndex = (currentIndex + 1) % MEASUREMENT_CONTAINER_IDS.length;
     const nextId = MEASUREMENT_CONTAINER_IDS[nextIndex];
 
-    const currentEle = document.getElementById(currentId);
-    const nextEle = document.getElementById(nextId);
-
-    currentEle.style.visibility = 'hidden';
-    nextEle.style.visibility = 'visible';
+    setMeasurementsVisibility(currentId, nextId);
+    setArcColours(nextId);
     currentIndex = nextIndex;
   };
 
--- common/utils.js
+++ common/utils.js
@@ -8,4 +8,18 @@ export const zeroPad = (i) => {
 
 export const MONTH_ABBREVIATIONS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 export const DAY_ABBREVIATIONS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-export const MEASUREMENT_CONTAINER_IDS = ['steps-container', 'heart-rate-container', 'calories-container'];
+
+const stepsId = 'steps-container';
+const heartRateId = 'heart-rate-container';
+const caloriesId = 'calories-container';
+export const MEASUREMENT_CONTAINER_IDS = [stepsId, heartRateId, caloriesId];
+export const BACKGROUND_ARC_COLOURS = {
+  [stepsId]: '#003366',
+  [heartRateId]: '#3f0000',
+  [caloriesId]: '#512f11'
+};
+export const ARC_COLOURS = {
+  [stepsId]: '#0080ff',
+  [heartRateId]: '#ff0000',
+  [caloriesId]: '#ffa500'
+};
