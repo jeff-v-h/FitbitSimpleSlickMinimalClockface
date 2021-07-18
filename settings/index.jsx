f4f2c814d23c30f36a8f0ef923239cf5b8354dad
jeff-h <jeffvh@outlook.com>
2021-07-18 17:25:36 +1000

    add settings sections to settings ui

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,24 +1,24 @@
+import { settingsKeys } from '../common/constants';
+
+const getSectionTitle = (title) => (
+  <Text bold align="left">
+    {title}
+  </Text>
+);
+
 function SettingsPage(props) {
   return (
     <Page>
-      <Section
-        title={
-          <Text bold align="left">
-            Background
-          </Text>
-        }
-      >
-        <ColorSelect settingsKey="background-colour" colors={[{ color: '#000000' }, { color: '#ffffff' }]} />
-      </Section>
-      <Section
-        title={
-          <Text bold align="left">
-            Date
-          </Text>
-        }
-      >
+      <Section title={getSectionTitle('Background')}>
         <ColorSelect
-          settingsKey="text-colour"
+          settingsKey={settingsKeys.backgroundColour}
+          colors={[{ color: '#000000' }, { color: '#ffffff' }]}
+        />
+      </Section>
+      <Section title={getSectionTitle('Seconds Circle')}>
+        <Toggle settingsKey={settingsKeys.dynamicSecondsColour} label="Same colour as measurement shown" />
+        <ColorSelect
+          settingsKey={settingsKeys.secondsColour}
           colors={[
             { color: '#000000' },
             { color: '#ffffff' },
@@ -28,15 +28,9 @@ function SettingsPage(props) {
           ]}
         />
       </Section>
-      <Section
-        title={
-          <Text bold align="left">
-            Time
-          </Text>
-        }
-      >
+      <Section title={getSectionTitle('Text')}>
         <ColorSelect
-          settingsKey="text-colour"
+          settingsKey={settingsKeys.textColour}
           colors={[
             { color: '#000000' },
             { color: '#ffffff' },
@@ -46,22 +40,15 @@ function SettingsPage(props) {
           ]}
         />
       </Section>
-      <Section
-        title={
-          <Text bold align="left">
-            Measurement
-          </Text>
-        }
-      >
-        <ColorSelect
-          settingsKey="text-colour"
-          colors={[
-            { color: '#000000' },
-            { color: '#ffffff' },
-            { color: '#0080ff' },
-            { color: '#ff0000' },
-            { color: '#ffa500' }
-          ]}
+      <Section title={getSectionTitle('Measurements')}>
+        <AdditiveList
+          settingsKey={settingsKeys}
+          addAction={
+            <Select
+              label="Select measurements"
+              options={[{ name: 'Heart rate' }, { name: 'Steps' }, { name: 'Calories' }]}
+            />
+          }
         />
       </Section>
     </Page>
