74c071dc60ad5c4de9573495c956730ca5d266b4
jeff-h <jeffvh@outlook.com>
2021-07-31 18:01:44 +1000

    measurement text colour is abel to be set dynamically based on measurement

--- app/setup/click-events.js
+++ app/setup/click-events.js
@@ -1,19 +1,23 @@
-import * as document from 'document';
 import state from '../../common/state';
 import { DARK_MEASUREMENT_COLOURS, MEASUREMENT_COLOURS } from '../../common/constants';
+import { getElementById, getElementsByClassName } from '../../common/utils';
 
 const initiateClickEvents = () => {
-  const rootElement = document.getElementById('root');
-  const secondsBackgroundArc = document.getElementById('seconds-background-arc');
-  const secondsArc = document.getElementById('seconds-arc');
+  const rootElement = getElementById('root');
+  const secondsBackgroundArc = getElementById('seconds-background-arc');
+  const secondsArc = getElementById('seconds-arc');
+  const measurementTextElements = getElementsByClassName('unit');
 
   const setMeasurementsVisibility = (currentId, nextId) => {
-    const currentEle = document.getElementById(currentId);
-    const nextEle = document.getElementById(nextId);
+    const currentEle = getElementById(currentId);
+    const nextEle = getElementById(nextId);
 
     currentEle.style.visibility = 'hidden';
     nextEle.style.visibility = 'visible';
   };
+
+  const setMeasurementTextColours = (measurementContainerId) =>
+    measurementTextElements.forEach((e) => (e.style.fill = MEASUREMENT_COLOURS[measurementContainerId]));
 
   const setArcColours = (measurementContainerId) => {
     secondsBackgroundArc.style.fill = DARK_MEASUREMENT_COLOURS[measurementContainerId];
@@ -21,15 +25,21 @@ const initiateClickEvents = () => {
   };
 
   const displayNextActvity = () => {
-    const { measurementContainerIds, currentMeasurementIndex, isDynamicSecondsColour } = state;
+    const { measurementContainerIds, currentMeasurementIndex, isDynamicSecondsColour, isDynamicMeasurementTextColour } =
+      state;
     const currentId = measurementContainerIds[currentMeasurementIndex];
     const nextIndex = (currentMeasurementIndex + 1) % measurementContainerIds.length;
     const nextId = measurementContainerIds[nextIndex];
 
     setMeasurementsVisibility(currentId, nextId);
+    if (isDynamicMeasurementTextColour) {
+      setMeasurementTextColours(nextId);
+    }
+
     if (isDynamicSecondsColour) {
       setArcColours(nextId);
     }
+
     state.currentMeasurementIndex = nextIndex;
   };
 
